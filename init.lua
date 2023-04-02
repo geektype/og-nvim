2868b3f57d9076ea81872b55a5a2639fcb0aa2d6
psyaa23 <psyaa23@nottingham.ac.uk>
2023-04-02 13:06:36 +0100

    Open nvim-tree on startup

--- init.lua
+++ init.lua
@@ -7,3 +7,10 @@ vim.g.loaded_netrwPlugin = 1
 
 -- set termguicolors to enable
 vim.opt.termguicolors = true
+
+local function open_nvim_tree()
+
+  -- open the tree
+  require("nvim-tree.api").tree.open()
+end
+vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
