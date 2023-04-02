1c8add6442e3fc3eba3f000de832f22f6312a812
psyaa23 <psyaa23@nottingham.ac.uk>
2023-04-02 15:35:23 +0100

    Bootstrap packer

--- init.lua
+++ init.lua
@@ -1,6 +1,6 @@
+require("ocam.packer")
 require("ocam.remap")
 require("ocam.set")
-require("ocam.packer")
 
 -- disable netrw
 vim.g.loaded_netrw = 1
--- lua/ocam/packer.lua
+++ lua/ocam/packer.lua
@@ -1,4 +1,16 @@
 -- This file can be loaded by calling `lua require('plugins')` from your init.vim
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
+
+local packer_bootstrap = ensure_packer()
 
 -- Only required if you have packer configured as `opt`
 vim.cmd [[packadd packer.nvim]]
@@ -65,5 +77,8 @@ return require('packer').startup(function(use)
 
     use {'akinsho/bufferline.nvim', tag = "v3.*", requires = 'nvim-tree/nvim-web-devicons'}
     use { "catppuccin/nvim", as = "catppuccin" }
+    if packer_bootstrap then
+        require('packer').sync()
+    end
 end)
 
